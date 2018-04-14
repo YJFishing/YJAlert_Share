96280991fb9ae6a0886bdc0c9f9cb15a6b5c1ed4
YJCoder <763935280@qq.com>
2018-04-15 00:29:57 +0800

    YJAlertView  property

--- YJAlert/YJAlertView.m
+++ YJAlert/YJAlertView.m
@@ -7,6 +7,7 @@
 //
 
 #import "YJAlertView.h"
+#import "YJAlertTextView.h"
 
 #define YJAlertScreenW ([UIScreen mainScreen].bounds.size.width)
 #define YJAlertScreenH ([UIScreen mainScreen].bounds.size.width)
@@ -45,6 +46,37 @@ static CGFloat const YJAlertSheetTitleMargin = 6;
     BOOL Showed;
     BOOL enableDeallocLog;
 }
+@property (nonatomic, strong) UIView *contentView;
+@property (nonatomic, strong) UIView *sheetContainerView;
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UICollectionViewFlowLayout *flowLayout;
+@property (nonatomic, strong) UICollectionViewFlowLayout *flowLayout2;
+@property (nonatomic, strong) UICollectionView *collectionView;
+@property (nonatomic, strong) UICollectionView *collectionView2;
+@property (nonatomic, strong) UIPageControl *pageControl;
+@property (nonatomic, strong) UIButton *cancelButton;
+@property (nonatomic, strong) UIButton *collectionButton;
+@property (nonatomic, strong) UIButton *dismissButton;
+@property (nonatomic, strong) NSMutableArray *actions;
+@property (nonatomic, strong) NSMutableArray *actions2;
+@property (nonatomic, assign) YJAlertActionStyle alertStyle;
+@property (nonatomic, strong) NSString *alertTitle;
+@property (nonatomic, strong) NSAttributedString *alertAttributedTitle;
+@property (nonatomic, strong) NSString *message;
+@property (nonatomic, strong) NSAttributedString *attirbutedMessage;
+@property (nonatomic ,strong) UIView *plainView;
+//palin样式添加自定义的titleView
+@property (nonatomic, strong) UIView *customPlainTitleView;
+//collection样式添加自定义的titleView的父视图
+@property (nonatomic, strong) UIScrollView *customPlainTitleScrollView;
+@property (nonatomic , strong) UIView *customHUD;
+@property (nonatomic, strong) UIView *textContainerView;
+@property (nonatomic, strong) UIView *bottomLineView;
+@property (nonatomic, strong) YJAlertTextView *titleTextView;
+@property (nonatomic, strong) YJAlertTextView *messageTextView;
+@property (nonatomic ,strong) UIScrollView *scrollView;
+@property (nonatomic, strong) UIView *titleContentView;
+@property (nonatomic, copy) void (^dismissComplete)(void);
 @end
 
 
