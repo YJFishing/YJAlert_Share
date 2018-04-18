5af8d7b1be499bfeaccd213a134832ef378c21ae
YJCoder <763935280@qq.com>
2018-04-18 18:09:54 +0800

    懒加载  初始化

--- YJAlert/YJAlertView.m
+++ YJAlert/YJAlertView.m
@@ -9,6 +9,7 @@
 #import "YJAlertView.h"
 #import "YJAlertTextView.h"
 #import "YJAlertTableViewCell.h"
+#import "YJAlertCollectionViewCell.h"
 
 #define YJAlertScreenW ([UIScreen mainScreen].bounds.size.width)
 #define YJAlertScreenH ([UIScreen mainScreen].bounds.size.width)
@@ -43,6 +44,7 @@ static CGFloat const YJAlertSheetTitleMargin = 6;
     CGFloat _iPhoneXLandScapeTextMargin;
     CGFloat YJAlertSeparatorLineWH;
     CGFloat PlainViewWidth;
+    CGFloat CancelMargin;
     BOOL AutoAdjustHomeIndicator;
     BOOL Showed;
     BOOL enableDeallocLog;
@@ -294,6 +296,7 @@ static CGFloat const YJAlertSheetTitleMargin = 6;
 - (UIButton *)cancelButton {
     if (!_cancelAction) {
         UIButton *cancelButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        [self.scrollView addSubview:cancelButton];
         cancelButton.backgroundColor = [UIColor colorWithRed:247/255.0 green:247/255.0 blue:247/255.0 alpha:0.7];
         cancelButton.titleLabel.font = [UIFont systemFontOfSize:17];
         [cancelButton setTitleColor:[UIColor colorWithRed:0.2 green:0.2 blue:0.2 alpha:1] forState:UIControlStateNormal];
@@ -302,4 +305,222 @@ static CGFloat const YJAlertSheetTitleMargin = 6;
     }
     return _cancelButton;
 }
+
+- (UIButton *)collectionButton {
+    if (_collectionButton) {
+        UIButton *collectionButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        collectionButton.backgroundColor = [UIColor colorWithRed:247/255.0 green:247/255.0 blue:247/255.0 alpha:0.7];
+        [self.scrollView addSubview:collectionButton];
+        collectionButton.titleLabel.font = [UIFont systemFontOfSize:17];
+        [collectionButton setTitleColor:[UIColor colorWithRed:0.2 green:0.2 blue:0.2 alpha:1] forState:UIControlStateNormal];
+        [_collectionButton addTarget:self action:@selector(collectionButtonClicked) forControlEvents:UIControlEventTouchUpInside];
+        _collectionButton = collectionButton;
+    }
+    return _collectionButton;
+}
+- (void)collectionButtonClicked {
+    !self.collectionAction.handler ? : self.collectionAction.handler(self.collectionAction);
+    [self dismiss];
+}
+
+- (UIPageControl *)pageControl {
+    if (!_pageControl) {
+        UIPageControl *pageControl = [[UIPageControl alloc] init];
+         pageControl.backgroundColor = [UIColor colorWithRed:247/255.0 green:247/255.0 blue:247/255.0 alpha:0.7];
+         pageControl.pageIndicatorTintColor = [UIColor colorWithRed:217/255.0 green:217/255.0 blue:217/255.0 alpha:1];
+        pageControl.currentPageIndicatorTintColor = [UIColor colorWithRed:102/255.0 green:102/255.0 blue:102/255.0 alpha:1];
+        [self.scrollView addSubview:pageControl];
+        _pageControl = pageControl;
+    }
+    return _pageControl;
+}
+
+- (UICollectionView *)collectionView {
+    if (!_collectionView) {
+        [self.sheetContainerView insertSubview:self.scrollView atIndex:1];
+        self.scrollView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, YJAlertAdjustHomeIndicatorHeight, 0);
+        self.scrollView.translatesAutoresizingMaskIntoConstraints = NO;
+        NSArray *scrollViewCons1 = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[scrollView]-0-|" options:0 metrics:nil views:@{@"scrollView" : self.scrollView}];
+        [self addConstraints:scrollViewCons1];
+        NSArray *scrollViewCons2 = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[scrollView]-0-|" options:0 metrics:nil views:@{@"scrollView":self.scrollView}];
+        [self addConstraints:scrollViewCons2];
+        
+        //title和message的容器View
+        self.textContainerView.backgroundColor = [UIColor colorWithRed:247/255.0 green:247/255.0 blue:247/255.0 alpha:0.7];
+        [self.scrollView addSubview:self.textContainerView];
+        [self.textContainerView addSubview:self.titleTextView];
+        
+        UICollectionViewFlowLayout *flowlayout = [[UICollectionViewFlowLayout alloc] init];
+        flowlayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+        _flowLayout = flowlayout;
+        
+        UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.textContainerView.frame), YJAlertScreenW, 80) collectionViewLayout:flowlayout];
+        collectionView.backgroundColor = [UIColor colorWithRed:247/255.0 green:247/255.0 blue:247/255.0 alpha:0.7];
+        collectionView.showsHorizontalScrollIndicator = NO;
+        collectionView.showsVerticalScrollIndicator = NO;
+        
+        collectionView.dataSource = self;
+        collectionView.delegate = self;
+        
+        collectionView.scrollsToTop = NO;
+        
+        [collectionView registerClass:[YJAlertCollectionViewCell class] forCellWithReuseIdentifier:NSStringFromClass([YJAlertCollectionViewCell class])];
+        if (@available(iOS 11.0,*)) {
+            [collectionView performSelector:@selector(setContentInsetAdjustmentBehavior:) withObject:@(2)];
+        }
+        [self.scrollView addSubview:collectionView];
+        [self cancelButton];
+        _collectionView = collectionView;
+    }
+    return _collectionView;
+}
+
+- (UICollectionView *)collectionView2 {
+    if (!_collectionView2) {
+        UICollectionViewFlowLayout *flowlayout = [[UICollectionViewFlowLayout alloc] init];
+        flowlayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+        _flowLayout2 = flowlayout;
+        
+        UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:_flowLayout2];
+        collectionView.backgroundColor = [UIColor colorWithRed:247/255.0 green:247/255.0 blue:247/255.0 alpha:0.7];
+        collectionView.showsVerticalScrollIndicator = NO;
+        collectionView.showsHorizontalScrollIndicator = NO;
+        
+        collectionView.dataSource = self;
+        collectionView.delegate = self;
+        collectionView.scrollsToTop = NO;
+        
+        [collectionView registerClass:[YJAlertCollectionViewCell class] forCellWithReuseIdentifier:NSStringFromClass([YJAlertCollectionViewCell class])];
+        if (@available(iOS 11.0,*)) {
+            [collectionView performSelector:@selector(setContentInsetAdjustmentBehavior:) withObject:@(2)];
+        }
+        [self.scrollView addSubview:collectionView];
+        _collectionView2 = collectionView;
+    }
+    return _collectionView2;
+}
+
+- (UIView *)plainView {
+    if (!_plainView) {
+        UIView *plainView = [[UIView alloc] init];
+        plainView.clipsToBounds = YES;
+        plainView.layer.cornerRadius = 8;
+        plainView.backgroundColor = [UIColor whiteColor];
+        plainView.frame = CGRectMake((YJAlertScreenW - PlainViewWidth) * 0.5, (YJAlertScreenH - 200), PlainViewWidth, 200);
+        
+        [plainView addSubview:self.textContainerView];
+        [self.textContainerView addSubview:self.titleTextView];
+        [self.textContainerView addSubview:self.messageTextView];
+        self.dismissButton.userInteractionEnabled = NO;
+        
+        [plainView addSubview:self.scrollView];
+        if (_alertStyle == YJAlertStyleStylePlain) {
+            UIView *bottomLineView = [[UIView alloc] init];
+            bottomLineView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.2];
+            [self.textContainerView addSubview:bottomLineView];
+            _bottomLineView = bottomLineView;
+        }
+        [self addSubview:plainView];
+        _plainView = plainView;
+    }
+    return _plainView;
+}
+
+#pragma mark ---初始化---
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self initialization];
+    }
+    return self;
+}
+
+- (void)initialization {
+    _textViewUserInteractionEnabled = YES;
+    AutoAdjustHomeIndicator = YES;
+    _dismissTimeInterval = 1;
+    PlainViewWidth = 290;
+    
+    YJAlertSeparatorLineWH = (0.5 / [UIScreen mainScreen].scale);
+    YJAlertSeparatorLineWH = YJAlertSeparatorLineWH < 0.25 ? 0.25 : YJAlertSeparatorLineWH;
+    
+    _iPhoneXLandScapeTextMargin = (YJAlertIsiPhoneX && YJAlertScreenW > YJAlertScreenH ? 44 : 0);
+    textContatinerViewCurrentMaxH_ = (YJAlertScreenH - 100 - YJAlertButtonH * 4);
+    TBMargin = 15;
+    
+    CancelMargin = (YJAlertScreenW > 321 ? 7 : 5);
+    enableDeallocLog = NO;
+    
+    self.titleTextViewAlignment = NSTextAlignmentCenter;
+    self.messageTextViewAlignment = NSTextAlignmentCenter;
+    self.textViewLeftRightMargin = 15;
+    self.flowLayoutItemWidth = 76;
+    
+    UIView *contentView = [[UIView alloc] init];
+    contentView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0];
+    [self insertSubview:contentView atIndex:0];
+    self.contentView = contentView;
+    
+    contentView.translatesAutoresizingMaskIntoConstraints = NO;
+    NSArray *contentViewCons1 = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[contentView]-0-|" options:0 metrics:nil views:@{@"contentView":contentView}];
+    [self addConstraints:contentViewCons1];
+    NSArray *contentViewCons2 = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[contentView]-0-|" options:0 metrics:nil views:@{@"contentView":contentView}];
+    [self addConstraints:contentViewCons2];
+    
+    UIButton *dismissButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    dismissButton.backgroundColor = [UIColor clearColor];
+    [self.contentView insertSubview:dismissButton atIndex:0];
+    self.dismissButton = dismissButton;
+    
+    dismissButton.translatesAutoresizingMaskIntoConstraints = NO;
+    NSArray *dismissButtonCons1 = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[dismissButton]-0-|" options:0 metrics:nil views:@{@"dismissButton":dismissButton}];
+    [self.contentView addConstraints:dismissButtonCons1];
+    [dismissButton addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationChanged:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(dismiss) name:YJAlertDismissNotification object:nil];
+}
+
+#pragma mark ---监听屏幕旋转---
+- (void)orientationChanged:(NSNotification *)notification {
+    
+}
+#pragma mark ---更改frame---
+- (void)setFrame:(CGRect)frame {
+    frame = CGRectMake(0, 0, YJAlertScreenW, YJAlertScreenH);
+    [super setFrame:frame];
+}
+
+#pragma mark ---UITableViewDataSource---
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return self.alertStyle == YJAlertStyleActionSheet ? 2: 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return section == 0 ? self.actions.count : 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    YJAlertTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([YJAlertTableViewCell class]) forIndexPath:indexPath];
+    if (indexPath.section == 0) {
+        cell.action = self.actions[indexPath.row];
+    }else {
+        cell.action = self.cancelAction;
+    }
+    return cell;
+}
+
+#pragma mark ---UITableViewDelegate---
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    YJAlertAction *action = indexPath.section == 0 ? self.actions[indexPath.row] : self.cancelAction;
+    return action.rowHeight;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return CGFLOAT_MIN;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return (section == 0) ? CancelMargin : CGFLOAT_MIN;
+}
+
+
 @end
